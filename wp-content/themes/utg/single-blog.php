a3bb962d1169a0f9984977fe9cd272b2790513fc
dmitriy-stativka <dmitriy.stativka@catenamedia.com>
2019-10-28 14:17:11 +0200

    created all blog pages

--- wp-content/themes/utg/single-blog.php
+++ wp-content/themes/utg/single-blog.php
@@ -6,21 +6,24 @@
 
 <div class="blog-single">
     <div class="flex_container">
-        <div class="blog-single-img">
-            <img src="<?php echo $image_blog['url'];?>" alt="">
-        </div>
-        <div class="blog-single-in">
-            
-            
-            <div class="blog_social">
-                
+        <div class="blog-container">
+            <div class="blog-single-img">
+                <img src="<?php echo $image_blog['url'];?>" alt="">
             </div>
-            <div class="blog_description">
-                <h1 class="blog-title"><?php the_title();?></h1>
-                <?php echo $text_blog;?>
+            <div class="blog-single-in">
+                <div class="blog_social">
+                    <span class="social-text">Поделитесь статьёй в соц. сетях:</span>
+                    <?php echo do_shortcode('[addtoany]');?>
+                </div>
+                <div class="blog_description">
+                    <h1 class="blog-title"><?php the_title();?></h1>
+                    <span class="blog_date"><?php echo get_the_date();?></span>
+                    <?php echo $text_blog;?>
+                    
+                </div>
             </div>
-
         </div>
+        
 
     </div>
 </div>
