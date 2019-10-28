0495176de8b0b04fdfbd36e0c58f3f890721a819
dmitriy-stativka <dmitriy.stativka@catenamedia.com>
2019-10-28 12:29:46 +0200

    the last commit of blog

--- wp-content/themes/utg/single-blog.php
+++ wp-content/themes/utg/single-blog.php
@@ -1,7 +1,29 @@
 <?php get_header();?>
 
-<?php the_title();?>
+<?php $image_blog = get_field('image_blog');
+      $text_blog  = get_field('text_blog');?>
 
-<?php the_content();?>
+
+<div class="blog-single">
+    <div class="flex_container">
+        <div class="blog-single-img">
+            <img src="<?php echo $image_blog['url'];?>" alt="">
+        </div>
+        <div class="blog-single-in">
+            
+            
+            <div class="blog_social">
+                
+            </div>
+            <div class="blog_description">
+                <h1 class="blog-title"><?php the_title();?></h1>
+                <?php echo $text_blog;?>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
 
 <?php get_footer();?>
